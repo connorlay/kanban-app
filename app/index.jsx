42e0e65306cbbc5b9addcf9c136b95f7af0adb4a
Connor Lay <connorlay@Connors-MacBook-Pro.local>
2015-10-25 21:43:23 -0700

    [add] localstorage support

--- app/index.jsx
+++ app/index.jsx
@@ -3,11 +3,17 @@ import './main.css'
 import React from 'react'
 import App from './components/App.jsx'
 
+import alt from './libs/alt'
+import storage from './libs/storage'
+import persist from './libs/persist'
+
 import 'array.prototype.findindex'
 
 main()
 
 function main () {
+  persist(alt, storage, 'app')
+
   const app = document.createElement('div')
 
   document.body.appendChild(app)
